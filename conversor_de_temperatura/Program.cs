733f5fb0f9abac0bb46bf258c4a8ed5ebe8bdd0c
GR Silva <grscookie@gmail.com>
2022-01-19 13:04:16 -0300

    first commit

---
+++ conversor_de_temperatura/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conversor_de_temperatura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a temperatura em Celsius: ");
+            double c = double.Parse(Console.ReadLine());
+
+            double f = (c * 9 / 5) + 32;
+            double k = c + 273.15;
+
+            Console.WriteLine();
+
+            Console.WriteLine("Temperatura em Fahrenheit: " + f + " ºF");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Temperatura em Kelvin: " + k + " K");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Pressione qualquer tecla para finalizar.");
+            Console.ReadKey();
+        }
+    }
+}
